2381deb89d2b2b7ef58a3fa4fca790548b67585a
ishowfun <236841637@qq.com>
2018-07-29 00:23:18 +0800

    Site updated: 2018-07-29 00:23:12

---
+++ js/music.js
@@ -0,0 +1,19 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,    
+    audio: [
+      {
+        name: "freedom",
+        artist: 'Anthony Hamilton/Elayna Boynton',
+        url: 'https://tencii.com/��Ŀ����/����������git����/freedom.mp3',
+        cover: 'https://tencii.com/images/likeqin.jpg',
+      },
+      {
+        name: '�°�Сҹ��',
+        artist: '�����',
+        url: 'https://tencii.com/��Ŀ����/����������git����/�°�Сҹ��.mp3',
+        cover: 'https://tencii.com/images/likeqin.jpg',
+      }
+    ]
+});
