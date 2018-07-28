0f99f9aca274da4018b2ebc08f0cfe6a6c5d3e79
ishowfun <236841637@qq.com>
2018-07-29 00:43:57 +0800

    Site updated: 2018-07-29 00:43:49

--- js/music.js
+++ js/music.js
@@ -7,13 +7,13 @@ const ap = new APlayer({
         name: "freedom",
         artist: 'Anthony Hamilton/Elayna Boynton',
         url: 'https://tencii.com/项目管理/禅道社区版git集成/freedom.mp3',
-        cover: 'https://tencii.com/images/likeqin.jpg',
+        cover: 'https://tencii.com/images/freedom.jpg',
       },
       {
         name: '月半小夜曲',
         artist: '李克勤',
         url: 'https://tencii.com/项目管理/禅道社区版git集成/月半小夜曲.mp3',
-        cover: 'https://tencii.com/images/likeqin.jpg',
+        cover: 'https://tencii.com/images/freedom.jpg',
       }
     ]
 });
